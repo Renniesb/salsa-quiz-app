bdfd4238415b1a8fe08fc309c6dd7932743fd823
Rennie Bevineau <rsbevineau@gmail.com>
2020-04-10 21:05:57 -0400

    fixed bugs in javascript

--- script.js
+++ script.js
@@ -6,47 +6,64 @@ const questions = [
   },
   {
     questionText: "Whats your name?",
-    choices: ["John", "Jane", "ughg"],
+    choices: ["John", "Rennie", "ughg"],
     correctAnswer: "John"
   },
   {
     questionText: "Whats your name?",
-    choices: ["John", "Jane", "ughg"],
+    choices: ["John", "Scott", "ughg"],
     correctAnswer: "John"
   }
 ];
 
 let currentQuestionIndex = 0;
-let currentQuestion = questions[currentQuestionIndex];
 let correct = 0;
 let incorrect = 0;
-let totalQuestions = questions.length();
+let totalQuestions = questions.length;
 let questionPercentage = 0;
+let choiceId = "";
 
-function displayQuestion(question) {
-  // write code to display the question
-  let html = `<div>
-  <p> Question ${currentQuestionIndex + 1} of ${totalQuestions}</p>
-  <p>${correct} correct, ${incorrect} incorrect</p>
-  <p>${question.questionText}</p>
-  <form>
-    <input type=radio value=${question.choices[0]} />
-  </form>
-  </div>`;
+function displayQuestion() {
+  hideAllPages();
+  hideAllButtons();
+  $(".question-screen").show();
+  let questionHtml = `
+  <p>Question ${currentQuestionIndex + 1} of ${totalQuestions}</p>
+  <p class="score">${correct} correct, ${incorrect} incorrect</p>
+  <p>${questions[currentQuestionIndex].questionText}</p>
+  <form action="">
+  ${showMultipleChoiceOptions()}
+  </form>  
+  `;
 
-  $("div").html(html);
-  $(".submit-question").on("click", function() {
+  $(".question-info").html(questionHtml);
+  $("#submit-answer").show();
+  $("#submit-answer").on("click", function(event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
     onAnswerSubmit();
   });
 }
 
 function displayEndPage() {
   //do total right percentage calculation
-  questionPercentage = correct / totalQuestions;
+  questionPercentage = Math.floor((correct / totalQuestions) * 100);
   //show a div
   //get the percentage right div with jquery and change value
+  hideAllPages();
+  hideAllButtons();
+  $(".start-end-screen").show();
+  $(".start-end-info").html(
+    `<h2>Salsa Dance Quiz</h2>
+    <p>${correct} of ${totalQuestions} correct</p>
+    <p>${questionPercentage}%</p>
+    <p>Thanks for playing</p>`
+  );
+
+  $("#start-new-quiz").show();
   $("#start-new-quiz").on("click", function() {
-    hideAllPages();
+    event.preventDefault();
+    event.stopImmediatePropagation();
     resetQuizValues();
     showStartPage();
   });
@@ -54,44 +71,64 @@ function displayEndPage() {
 
 function onAnswerSubmit() {
   // write code to check if the user submitted answer is the correct answer
-  let userAnswer = $("#choice").val();
-  let currentQuestion = questions[currentQuestionIndex];
-  if (userAnswer == currentQuestion.correctAnswer) {
-    // do something
-    // update correct scores
+  let userAnswer = $("input[name='dance-type']:checked").val();
+  if (userAnswer == questions[currentQuestionIndex].correctAnswer) {
     correct = correct + 1;
     // update correct value with jquery
-
+    $(".score").html(`${correct} correct, ${incorrect} incorrect`);
     // show correct badge with jquery
+    $(".feedback").html(`<span class="correct">Correct</span>`);
   } else {
     // do something else
-    incorrect = incorrect - 1;
-
-    // update incorrect value with jquery
+    incorrect = incorrect + 1;
+    $(".score").html(`${correct} correct, ${incorrect} incorrect`);
 
     // show incorrect badge with jquery and correct answer
+    $(".feedback").html(`<span class="incorrect">Incorrect</span>`);
+    $(".correct-answer").html(
+      `Correct answer: ${questions[currentQuestionIndex].correctAnswer}`
+    );
   }
-  currentQuestionIndex = currentQuestionIndex + 1;
 
   //hide submit question, show next button
-
-  $("#next-button").on("click", function() {
+  hideAllButtons();
+  $("#next").show();
+  $("#next").on("click", function(event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    currentQuestionIndex = currentQuestionIndex + 1;
+    console.log("next loop", currentQuestionIndex);
     if (currentQuestionIndex === totalQuestions) {
+      emptyContents();
       displayEndPage();
     } else {
+      emptyContents();
       displayQuestion();
     }
   });
 }
 
 function startQuiz() {
-  currentQuestion = questions[currentQuestionIndex];
-  displayQuestion(currentQuestion);
+  displayQuestion();
 }
 
-function hideAllPages() {}
+function hideAllPages() {
+  $(".start-end-screen").hide();
+  $(".question-screen").hide();
+}
 
-function hideAllButtons() {}
+function hideAllButtons() {
+  $("#start-quiz").hide();
+  $("#start-new-quiz").hide();
+  $("#new-quiz").hide();
+  $("#submit-answer").hide();
+  $("#next").hide();
+}
+function emptyContents() {
+  $(".feedback").html(``);
+  $(".correct-answer").html(``);
+  $(".start-end-info").html("");
+}
 
 function resetQuizValues() {
   currentQuestionIndex = 0;
@@ -99,35 +136,42 @@ function resetQuizValues() {
   incorrect = 0;
   questionPercentage = 0;
 }
+function showMultipleChoiceOptions() {
+  let inputElement = "";
+  questions[currentQuestionIndex].choices.forEach(choice => {
+    inputElement =
+      inputElement +
+      `<label><input type="radio" name="dance-type" value=${choice} />
+            ${choice}</label><br />`;
+  });
+  console.log("multiple choice loop", currentQuestionIndex);
+  return inputElement;
+}
 
 function showStartPage() {
-  $(".start-button-div").show();
-
-  $("#start-button").on("click", function() {
-    hideAllPages();
+  hideAllPages();
+  hideAllButtons();
+  emptyContents();
+  $(".start-end-screen").show();
+  $("#start-quiz").show();
+  $("#start-quiz").on("click", function() {
+    event.preventDefault();
+    event.stopImmediatePropagation();
     startQuiz();
   });
 }
 
 $(showStartPage);
 
-//html on page
+// id=
+// start-quiz
+// new-quiz
+// submit-answer
+// next
 
-//start page
-//<h1>Salsa Dance Quiz</h1>
-//end page
-{
-  /* 
-<h2>Salsa Dance Quiz</h2>
-<p>4 of 5 correct</p>
-<p>80%</p>
-<p>Thanks for playing</p>
-<button>Start a new Quiz</button> 
-*/
-}
-//add feedback
-//<span class="correct">Correct</span>
-//<span class="incorrect">Incorrect</span>
-
-//create ids by taking out the spaces in a string
-//string.split(" ").join("")
+// class=
+// .start-end-screen
+// .question-screen
+// .feedback
+// .correct
+// .incorrect
